a7dd6890b67ed934188281672d4884668bab304f
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-09-02 14:41:26 +0300

    done basic tasks

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let unionType: string | number;
+unionType = "Hello";
+unionType = 30;
+
+type LiteralType = "enable" | "disable";
+let literalType: LiteralType;
+literalType = "enable";
+literalType = "disable";
